6db764f3fa6b0f03ca05f68a64c50015fb1cdbf0
VldKnd <kondratyev.w.i@gmail.com>
2024-04-14 16:48:34 +0200

    feat(street-side-data-models)

--- street-side-data-models/street_side/v1/data_models/document.py
+++ street-side-data-models/street_side/v1/data_models/document.py
@@ -12,24 +12,3 @@ class RemoteDocument(BaseModelWithHashId):
     year: Optional[str] = None
     quater: Optional[str] = None
     url: str
-
-    @functools.cached_property
-    def root_relative_path(self):
-        return (
-            f"{self.company_name}/" +\
-            f"{self.document_type}/" +\
-            (f"{self.year}/" if self.year is not None else "") +\
-            (f"{self.quater}/" if self.quater is not None else "")
-        )
-    
-    @functools.cached_property
-    def file_name(self):
-        _, file_extension = os.path.splitext(self.url)
-        return f"{self.hash_id}{file_extension}"
-    
-    def is_downloaded(self, path_to_root: str):
-        local_path = (
-            f"{path_to_root}"
-            f"{self.root_relative_path}"
-        )
-        return os.path.exists(local_path)
