5c910fd95573b18caad9a4b6096290ac0c8ea6a8
VldKnd <kondratyev.w.i@gmail.com>
2024-04-15 11:23:50 +0200

    feat(street-side-data-models)

--- street-side-data-models/street_side/v1/data_models/document.py
+++ street-side-data-models/street_side/v1/data_models/document.py
@@ -6,9 +6,14 @@ from venv import logger
 from street_side.v1.data_models.base import BaseModelWithHashId
 
 
+class DocumentType(BaseModelWithHashId):
+    name: str
+    yearly: bool
+    quaterly: bool
+
 class RemoteDocument(BaseModelWithHashId):
     company_name: str
-    document_type: str
+    document_name: str
     year: Optional[str] = None
     quater: Optional[str] = None
     url: str
