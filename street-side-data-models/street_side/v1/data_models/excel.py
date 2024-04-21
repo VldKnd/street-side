4e55d18a92d6f60a5acff079908e6cbc78ec1772
VldKnd <kondratyev.w.i@gmail.com>
2024-04-21 12:18:12 +0200

    feat(street-side-data-models): Add excel and response models

---
+++ street-side-data-models/street_side/v1/data_models/excel.py
@@ -0,0 +1,41 @@
+from typing import Dict, Hashable, List
+
+import pandas
+import pydantic
+
+
+class ExcelSheet(pydantic.BaseModel):
+    headers: List[Hashable]
+    rows: Dict[int, List[str]]
+
+
+class ExcelFile(pydantic.BaseModel):
+    sheets: Dict[str, ExcelSheet]
+    
+    @classmethod
+    def get_excel_file_from_local_disk(cls, path_to_file: str):
+        try:
+            excel_file = pandas.ExcelFile(path_to_file)
+        except Exception as e:
+            raise RuntimeError(f"Excel file parsing failed. {e}")
+        
+        sheet_names = excel_file.sheet_names
+        sheets = {}
+
+        for sheet_name in sheet_names:
+            excel_sheet_as_dataframe = excel_file.parse(sheet_name)
+            if excel_sheet_as_dataframe.empty: continue
+            excel_sheet_as_dict = excel_sheet_as_dataframe.astype(str).to_dict()
+            headers = list(excel_sheet_as_dict.keys())
+            indexes = excel_sheet_as_dict[headers[-1]].keys()
+            rows = {
+                index:[excel_sheet_as_dict[header][index] for header in headers]
+                for index in indexes
+            }
+
+            sheets[sheet_name] = ExcelSheet(
+                headers=headers,
+                rows=rows,
+            )
+
+        return cls(sheets=sheets)
---
+++ street-side-data-models/street_side/v1/data_models/response.py
@@ -0,0 +1,33 @@
+import datetime
+from typing import Optional
+
+import pydantic
+
+
+class CompanyGetRequestResponse(pydantic.BaseModel):
+    short_name: str
+    full_name: str
+    home_url: str
+    created_at: datetime.datetime | None
+    hash_id: str
+
+
+class DocumentTypeGetRequestResponse(pydantic.BaseModel):
+    company_hash_id: str
+    full_name: str
+    short_name: str
+    is_quaterly: bool
+    is_yearly: bool
+    created_at: datetime.datetime | None
+    hash_id: str
+
+class DocumentGetRequestResponse(pydantic.BaseModel):
+    document_type_id: str
+    date_published: Optional[datetime.datetime] = None
+    quater: Optional[str] = None
+    year: Optional[str] = None
+    pretty_date: str
+    remote_url: str
+    extension: str
+    created_at: datetime.datetime | None
+    hash_id: str
