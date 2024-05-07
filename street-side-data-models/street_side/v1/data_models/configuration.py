ecbf9be10d28e263ae604117c380824964357853
VldKnd <kondratyev.w.i@gmail.com>
2024-05-07 14:53:44 +0200

    chore(street-side-data-modles): Update dependencies

--- street-side-data-models/street_side/v1/data_models/configuration.py
+++ street-side-data-models/street_side/v1/data_models/configuration.py
@@ -1,28 +1,31 @@
 import os
 from typing import Dict
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings
 
 from street_side.v1.data_models.web import WebPage
 
 
-class StreetSideConfiguration(BaseModel):
+class SharedSettings(BaseSettings):
+    STREET_SIDE_DATASTORE_PATH: str = Field(default=...)
+    POSTGRES_DSN: str = Field(default=...)
+
+
+class ScrapperConfiguration(BaseModel):
     scrapper_web_pages: Dict[str, WebPage]
-    datastore_path: str
-    postgres_dsn: str
-    database_pool_size: int = 1
     
     @staticmethod
-    def from_env_json_file() -> "StreetSideConfiguration":
-        path_to_configuration_as_json = os.getenv("STREET_SIDE_CONFIGURATION_PATH")
+    def from_env_json_file() -> "ScrapperConfiguration":
+        path_to_configuration_as_json = os.getenv("SCRAPPER_CONFIGURATION_PATH")
 
         if path_to_configuration_as_json is None:
             raise RuntimeError(
-                "Enviroment variable STREET_SIDE_CONFIGURATION_PATH is not set."
+                "Enviroment variable SCRAPPER_CONFIGURATION_PATH is not set."
                 "Can not create configuration."
             )
         
         with open(path_to_configuration_as_json, 'r') as configuration_file:
             configuration_file_as_json = configuration_file.read()
 
-        return StreetSideConfiguration.model_validate_json(configuration_file_as_json)
+        return ScrapperConfiguration.model_validate_json(configuration_file_as_json)
