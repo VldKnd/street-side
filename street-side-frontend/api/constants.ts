0a51186fd8adec2251f77bdad15bff00ddfc86a5
VldKnd <kondratyev.w.i@gmail.com>
2024-05-10 15:46:14 +0200

    fix(street-side-frontend)

--- street-side-frontend/api/constants.ts
+++ street-side-frontend/api/constants.ts
@@ -1,5 +1,3 @@
-export const HOST = process.env.NEXT_PUBLIC_API_HOST;
-export const PORT = process.env.NEXT_PUBLIC_API_PORT;
-export const ADRESS = `${HOST}:${PORT}`;
+export const ADRESS = process.env.NEX_PUBLIC_API_ADRESS;
 export type fetchWithBodyInterface = [input: RequestInfo | URL, init?: RequestInit ]
 export const fetcher = (input: RequestInfo | URL, init?: RequestInit) => fetch(input, init).then(res => res.json())
